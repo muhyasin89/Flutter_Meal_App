98db05cf4dd0955ade5f7aa351a77f8f92bc2134
muhyasin89 <muhyasin89@gmail.com>
2020-05-03 12:14:29 +0700

    [UPDATE]first tutorial video

--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:rest_api/views/note_delete.dart';
 import '../models/note_for_listing.dart';
 import '../views/note_modify.dart';
 
@@ -37,10 +38,28 @@ class NoteList extends StatelessWidget {
       ),
       body: ListView.separated(
           itemBuilder: (_, index){
-            return ListTile(
-              title: Text(notes[index].noteTitle,
-              style: TextStyle(color: Theme.of(context).primaryColor)),
-              subtitle: Text("Last Edited on ${DateFormat("dd-MM-yyyy").format(notes[index].lastEditDateTime)}"),
+            return Dismissible(
+              key: ValueKey(notes[index].noteID),
+              direction: DismissDirection.startToEnd,
+              onDismissed: (direction){
+
+              },
+              confirmDismiss: (direction) async {
+                final result = await showDialog(
+                  context: context,
+                  builder: (_) => NoteDelete()
+                );
+                return result;
+              }, 
+              child: ListTile(
+                    title: Text(notes[index].noteTitle,
+                    style: TextStyle(color: Theme.of(context).primaryColor)),
+                    subtitle: Text("Last Edited on ${DateFormat("dd-MM-yyyy").format(notes[index].lastEditDateTime)}"),
+                    onTap: (){
+                      Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (_)=> NoteModify(noteID: notes[index].noteID)));
+                    },
+                  ), 
             );
           }, 
           separatorBuilder: (_, __)=> Divider(height: 1.0, color: Colors.green), 
